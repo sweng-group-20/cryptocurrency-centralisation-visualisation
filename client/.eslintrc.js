1e9c2da216f9e7468904d915ab81117cc487a430
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-03-02 20:49:08 +0000

    Add react boilerplate with linting and formatting

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier/prettier',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'jsx-a11y/href-no-hash': ['off'],
+    'prefer-arrow-callback': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'max-len': [
+      'warn',
+      {
+        tabWidth: 2,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+};
