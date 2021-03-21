0d64bc053758fcda1c248286168f7471661bac4b
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-03-21 01:36:58 +0000

    Add getPullRequestNumbers

--- server/src/http_clients/github.js
+++ server/src/http_clients/github.js
@@ -26,74 +26,51 @@ class GithubHttpClient extends BaseHttpClient {
   }
 
   /**
-   * @typedef {Object} PostResponseTypeFromPullRequestComments
-   * @property {Comments} comments
-   * @property {RateLimit} rateLimit
-   */
-
-  /**
-   * @typedef {Object} Comments
-   * @property {Node[]} nodes
-   */
-
-  /**
-   * @typedef {Object} Node
-   * @property {Object} author
-   * @property {String} createdAt
-   * @property {Number} databaseId
-   */
-
-  /**
-   * @typedef {Object} RateLimit
-   * @property {number} cost
-   * @property {number} remaining
-   */
-
-  /**
    * Retrieves all the comments of a pull request
-   * @param {string} repoOwner Name of the repo owner
-   * @param {*} repoName Name of the repo
-   * @param {*} pullRequestNumber Pull request number
-   * @returns {Promise<PostResponseTypeFromPullRequestComments>}
+   * @param {string} repoOwner Name of the repository owner
+   * @param {string} repoName Name of the repository
+   * @param {number} pullRequestNumber Pull request number
    */
   async getPullRequestComments(repoOwner, repoName, pullRequestNumber) {
     const commentsQuery = `
-    comments(first: 100) {
-      nodes {
-        databaseId
-        ...commentFields
+      comments(first: 100) {
+        nodes {
+          databaseId
+          ...commentFields
+        }
       }
-    }
     `;
     const query = `
-    query ($repoOwner: String!, $repoName:String!, $pullRequestNumber: Int!) {
-      repository(owner: $repoOwner, name: $repoName) {
-        pullRequest(number: $pullRequestNumber) {
-          ${commentsQuery}
-          reviewThreads(first: 100) {
-            nodes {
-              ${commentsQuery}
+      query ($repoOwner: String!, $repoName:String!, $pullRequestNumber: Int!) {
+        repository(owner: $repoOwner, name: $repoName) {
+          databaseId
+          pullRequest(number: $pullRequestNumber) {
+            databaseId
+            ${commentsQuery}
+            reviewThreads(first: 100) {
+              nodes {
+                ${commentsQuery}
+              }
             }
-          }
-          reviews(first: 100) {
-            nodes {
-              ${commentsQuery}
+            reviews(first: 100) {
+              nodes {
+                ${commentsQuery}
+              }
             }
           }
         }
+        rateLimit {
+          remaining
+          cost
+        }
       }
-      rateLimit {
-        remaining
-        cost
-      }
-    }
 
-    fragment commentFields on Comment {
-      createdAt
-      author {
-        login
+      fragment commentFields on Comment {
+        createdAt
+        author {
+          login
+        }
       }
-    }
     `;
     const variables = {
       repoOwner,
@@ -109,8 +86,13 @@ class GithubHttpClient extends BaseHttpClient {
 
     const { data } = respJson;
     const { rateLimit } = data;
-    const { pullRequest } = data.repository;
-    const { comments, reviewThreads, reviews } = pullRequest;
+    const { pullRequest, databaseId: repoDatabaseId } = data.repository;
+    const {
+      comments,
+      reviewThreads,
+      reviews,
+      databaseId: pullRequestDatabaseId,
+    } = pullRequest;
     let allCommentNodes = [...comments.nodes];
     reviewThreads.nodes.forEach(({ comments: reviewComments }) => {
       reviewComments.nodes.forEach((comment) => allCommentNodes.push(comment));
@@ -139,10 +121,96 @@ class GithubHttpClient extends BaseHttpClient {
     );
 
     return {
-      comments: {
-        nodes: allCommentNodes,
+      repository: {
+        databaseId: repoDatabaseId,
+        pullRequest: {
+          databaseId: pullRequestDatabaseId,
+          comments: {
+            nodes: allCommentNodes,
+          },
+        },
       },
       rateLimit,
+    };
+  }
+
+  /**
+   * Retrieves a list of pull request numbers
+   * @param {string} repoOwner Name of the repository owner
+   * @param {string} repoName Name of the repository
+   * @param {number} iterationLimit Limit of iterations during pagenation
+   * @param {string} startCursor Last synced cursor
+   */
+  async getPullRequestNumbers(
+    repoOwner,
+    repoName,
+    iterationLimit,
+    startCursor
+  ) {
+    const query = `
+      query ($repoOwner: String!, $repoName: String!, $cursor: String) {
+        repository(owner: $repoOwner, name: $repoName) {
+          pullRequests(first: 100, after: $cursor) {
+            nodes {
+              number
+            }
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+          }
+        }
+        rateLimit {
+          remaining
+          cost
+        }
+      }
+    `;
+    const variables = {
+      repoOwner,
+      repoName,
+      cursor: startCursor,
+    };
+
+    const nodes = [];
+    const rateLimit = {
+      remaining: null,
+      cost: 0,
+    };
+
+    let iterations = 0;
+    let hasNextPage = true;
+    while (hasNextPage && iterations < iterationLimit) {
+      // eslint-disable-next-line no-await-in-loop
+      const resp = await this.graphqlRequest(query, variables);
+      // eslint-disable-next-line no-await-in-loop
+      const respJson = await resp.json();
+      if (respJson.errors) {
+        throw new GraphQLError(JSON.stringify(respJson.errors));
+      }
+
+      const { rateLimit: currentRateLimit, repository } = respJson.data;
+      const { pullRequests } = repository;
+      const { pageInfo } = pullRequests;
+
+      rateLimit.remaining = currentRateLimit.remaining;
+      rateLimit.cost += currentRateLimit.cost;
+
+      pullRequests.nodes.forEach((node) => nodes.push(node));
+
+      hasNextPage = pageInfo.hasNextPage;
+      variables.cursor = pageInfo.endCursor;
+
+      iterations += 1;
+    }
+
+    return {
+      repository: {
+        pullRequests: {
+          nodes,
+        },
+        rateLimit,
+      },
     };
   }
 }
