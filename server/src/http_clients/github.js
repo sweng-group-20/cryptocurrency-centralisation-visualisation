1aad18c67e9c08b44aed05c353c95d2b42e9e1a6
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-03-21 23:31:30 +0000

    Add getIssueComments, refactor getPullRequestNumbers

--- server/src/http_clients/github.js
+++ server/src/http_clients/github.js
@@ -133,23 +133,114 @@ class GithubHttpClient extends BaseHttpClient {
     };
   }
 
+  async getIssueComments(repoOwner, repoName, issueNumber) {
+    const query = `
+      query ($repoOwner: String!, $repoName: String!, $issueNumber: Int!, $cursor: String) {
+        repository(owner: $repoOwner, name: $repoName) {
+          databaseId
+          issue(number: $issueNumber) {
+            databaseId
+            number
+            state
+            comments(first: 100, after: $cursor) {
+              nodes {
+                databaseId
+                createdAt
+                author {
+                  login
+                }
+              }
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+            }
+          }
+        }
+        rateLimit {
+          remaining
+          cost
+        }
+      }
+    `;
+    const variables = {
+      repoOwner,
+      repoName,
+      issueNumber,
+    };
+
+    const repository = {
+      databaseId: null,
+      issue: {
+        databaseId: null,
+        number: null,
+        state: null,
+        comments: {
+          nodes: [],
+        },
+      },
+      rateLimit: {
+        remaining: null,
+        cost: 0,
+      },
+    };
+
+    let hasNextPage = true;
+    while (hasNextPage) {
+      // eslint-disable-next-line no-await-in-loop
+      const resp = await this.graphqlRequest(query, variables);
+      // eslint-disable-next-line no-await-in-loop
+      const respJson = await resp.json();
+      if (respJson.errors) {
+        throw new GraphQLError(JSON.stringify(respJson.errors));
+      }
+
+      const { rateLimit, repository: issueRepository } = respJson.data;
+      const { cost, remaining } = rateLimit;
+      const { issue, databaseId: repositoryDatabaseId } = issueRepository;
+      const { databaseId: issueDatabaseId, number, state, comments } = issue;
+      const { nodes, pageInfo } = comments;
+
+      repository.databaseId = repositoryDatabaseId;
+
+      repository.issue.databaseId = issueDatabaseId;
+      repository.issue.number = number;
+      repository.issue.state = state;
+      nodes.forEach((node) => repository.issue.comments.nodes.push(node));
+
+      hasNextPage = pageInfo.hasNextPage;
+      variables.cursor = pageInfo.endCursor;
+
+      repository.rateLimit.remaining = remaining;
+      repository.rateLimit.cost += cost;
+    }
+
+    return repository;
+  }
+
   /**
-   * Retrieves a list of pull request numbers
+   * Retrieves a list of issue or pull request numbers
+   * @param {'pr' | 'issue'} type Type to retrieve numbers from
    * @param {string} repoOwner Name of the repository owner
    * @param {string} repoName Name of the repository
    * @param {number} pullRequestsLimit Number of pull requests to retrieve the number of
    * @param {string} startCursor Last synced cursor
    */
-  async getPullRequestNumbers(
+  async getIssueOrPullRequestNumbers(
+    type,
     repoOwner,
     repoName,
-    pullRequestsLimit,
+    limit,
     startCursor
   ) {
     const query = `
-      query ($repoOwner: String!, $repoName: String!, $first: Int!, $cursor: String) {
+      query ($repoOwner: String!, $repoName: String!, $first: Int!, $cursor: String, $states: [${
+        type === 'pr' ? 'PullRequestState!' : 'IssueState!'
+      }]) {
         repository(owner: $repoOwner, name: $repoName) {
-          pullRequests(first: $first, after: $cursor) {
+          ${
+            type === 'pr' ? 'pullRequests' : 'issues'
+          }(first: $first, after: $cursor, states: $states) {
             nodes {
               number
             }
@@ -171,22 +262,25 @@ class GithubHttpClient extends BaseHttpClient {
       cursor: startCursor,
     };
 
-    const nodes = [];
+    const states = ['CLOSED'];
+    if (type === 'pr') {
+      states.push('MERGED');
+    }
+
+    variables.states = states;
+
+    const pullRequestNodes = [];
     const rateLimit = {
       remaining: null,
       cost: 0,
     };
 
-    let numberOfPullRequestsRetrieved = 0;
+    let numberRetrieved = 0;
     let hasNextPage = true;
-    while (hasNextPage && numberOfPullRequestsRetrieved < pullRequestsLimit) {
-      const numberOfPullRequestsRemaining =
-        pullRequestsLimit - numberOfPullRequestsRetrieved;
-      const numberOfPullRequestsToGet =
-        numberOfPullRequestsRemaining < 100
-          ? numberOfPullRequestsRemaining
-          : 100;
-      variables.first = numberOfPullRequestsToGet;
+    while (hasNextPage && numberRetrieved < limit) {
+      const numberRemaining = limit - numberRetrieved;
+      const numberToGet = numberRemaining < 100 ? numberRemaining : 100;
+      variables.first = numberToGet;
 
       // eslint-disable-next-line no-await-in-loop
       const resp = await this.graphqlRequest(query, variables);
@@ -197,29 +291,78 @@ class GithubHttpClient extends BaseHttpClient {
       }
 
       const { rateLimit: currentRateLimit, repository } = respJson.data;
-      const { pullRequests } = repository;
-      const { pageInfo } = pullRequests;
+      let pageInfo = null;
+      let typeNodes = null;
+
+      if (type === 'pr') {
+        const { pullRequests } = repository;
+        pageInfo = pullRequests.pageInfo;
+        typeNodes = pullRequests.nodes;
+      } else {
+        const { issues } = repository;
+        pageInfo = issues.pageInfo;
+        typeNodes = issues;
+      }
 
       rateLimit.remaining = currentRateLimit.remaining;
       rateLimit.cost += currentRateLimit.cost;
 
-      pullRequests.nodes.forEach((node) => nodes.push(node));
+      typeNodes.forEach((node) => pullRequestNodes.push(node));
 
       hasNextPage = pageInfo.hasNextPage;
       variables.cursor = pageInfo.endCursor;
 
-      numberOfPullRequestsRetrieved += pullRequests.nodes.length;
+      numberRetrieved += typeNodes.length;
     }
 
-    return {
+    const response = {
       repository: {
-        pullRequests: {
-          nodes,
-        },
         endCursor: variables.cursor,
       },
       rateLimit,
     };
+
+    if (type === 'pr') {
+      response.repository.pullRequests = { nodes: pullRequestNodes };
+    } else {
+      response.repository.issues = { nodes: pullRequestNodes };
+    }
+
+    return response;
+  }
+
+  /**
+   * Retrieves a list of pull request numbers
+   * @param {string} repoOwner Name of the repository owner
+   * @param {string} repoName Name of the repository
+   * @param {number} pullRequestsLimit Maximum number of pull requests to retrieve the number of
+   * @param {string} startCursor Last synced cursor
+   */
+  getPullRequestNumbers(repoOwner, repoName, pullRequestsLimit, startCursor) {
+    return this.getIssueOrPullRequestNumbers(
+      'pr',
+      repoOwner,
+      repoName,
+      pullRequestsLimit,
+      startCursor
+    );
+  }
+
+  /**
+   * Retrieves a list of issue numbers
+   * @param {string} repoOwner Name of the repository owner
+   * @param {string} repoName Name of the repository
+   * @param {number} issuesLimit Maximum number of issues to retrieve the number of
+   * @param {string} startCursor Last synced cursor
+   */
+  getIssueNumbers(repoOwner, repoName, issuesLimit, startCursor) {
+    return this.getIssueOrPullRequestNumbers(
+      'issue',
+      repoOwner,
+      repoName,
+      issuesLimit,
+      startCursor
+    );
   }
 }
 
