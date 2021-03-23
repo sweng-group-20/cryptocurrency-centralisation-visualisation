133ebd0b6b8805167f1f4f8c0cb5f93b546095ff
aodhanocathain <78481710+aodhanocathain@users.noreply.github.com>
2021-03-23 15:23:08 +0000

    renamed api-docs.js to api_docs.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const apidocs = require('./routes/api-docs');
+const apidocs = require('./routes/api_docs.js');
 const routes = require('./routes/index');
 const { notFoundError, errorHandler } = require('./middlewares');
 
