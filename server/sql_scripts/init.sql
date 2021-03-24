ce3477850687e9895db4c2ccb978d86d3725e72c
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-03-24 01:17:21 +0000

    Add on delete cascade

--- server/sql_scripts/init.sql
+++ server/sql_scripts/init.sql
@@ -19,7 +19,7 @@ CREATE TABLE issues_and_pull_requests (
   PRIMARY KEY (database_id),
   CONSTRAINT check_issue_type CHECK (issue_type IN ('PR', 'ISSUE')),
   CONSTRAINT check_issue_state CHECK (issue_state IN ('OPEN', 'CLOSED', 'MERGED')),
-  CONSTRAINT fk_repository FOREIGN KEY (parent_repo_id) REFERENCES repositories (database_id)
+  CONSTRAINT fk_repository FOREIGN KEY (parent_repo_id) REFERENCES repositories (database_id) ON DELETE CASCADE
 );
 
 CREATE TABLE comments (
@@ -28,6 +28,6 @@ CREATE TABLE comments (
   author_login varchar(39),
   parent_issue_id bigint,
   PRIMARY KEY (database_id),
-  CONSTRAINT fk_parent_issue FOREIGN KEY (parent_issue_id) REFERENCES issues_and_pull_requests (database_id)
+  CONSTRAINT fk_parent_issue FOREIGN KEY (parent_issue_id) REFERENCES issues_and_pull_requests (database_id) ON DELETE CASCADE
 );
 
