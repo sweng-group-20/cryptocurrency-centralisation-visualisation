6a5e4ce554c2e93310790cbf662f18fdd7261f66
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-03-26 16:16:49 +0000

    Add api documentation

--- server/src/routes/bitcoin/application.js
+++ server/src/routes/bitcoin/application.js
@@ -5,7 +5,24 @@ const { getSatoshiIndex } = require('../../graph_data/satoshi_index');
 const router = express.Router();
 
 /**
- * Default response
+ * @openapi
+ *
+ * /bitcoin/application:
+ *   get:
+ *     description: Basic message for bitcoin application layer endpoint
+ *     tags:
+ *       - bitcoin
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   enum: ['application layer endpoint']
  */
 router.get('/', (_req, res) => {
   res.status(200);
@@ -14,6 +31,40 @@ router.get('/', (_req, res) => {
   });
 });
 
+/**
+ * @openapi
+ *
+ * /bitcoin/application/reference-client-concentration:
+ *   get:
+ *     description: Returns plot points for the reference client concentration factor in the application layer for Bitcoin - TEST EXECUTION MAY BE SLOW
+ *     tags:
+ *       - bitcoin
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                       data:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             x:
+ *                               type: string
+ *                               format: date
+ *                             y:
+ *                               type: number
+ */
 router.get('/reference-client-concentration', async (_req, res, next) => {
   try {
     const satoshiIndex = await getSatoshiIndex('bitcoin', 'bitcoin');
