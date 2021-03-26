3bdd2eea3255eddb956e67ad3f9c0bb40baa1ddb
Tim Kelly <tkelly3118@gmail.com>
2021-03-26 21:29:30 +0000

    added scroll arrow

--- client/src/components/ScrollArrow.jsx
+++ client/src/components/ScrollArrow.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ScrollArrow.css';
 import '../index.css';
 import Arrow from './images/arrow.png';
 
 function ScrollArrow() {
-  return <img className="ScrollArrow" src={Arrow} alt="" />;
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scorlled upto given distance
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 1000) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollBy(window.scrollBy(0, 650));
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+  }, []);
+
+  return (
+    <button type="button" onClick={scrollToTop}>
+      {isVisible && <img className="ScrollArrow" src={Arrow} alt="Go to top" />}
+    </button>
+  );
 }
 
 export default ScrollArrow;
