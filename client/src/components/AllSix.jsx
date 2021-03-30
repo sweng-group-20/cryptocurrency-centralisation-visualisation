39a3e42461534f6564605564ea6b84618f2f59be
Tim Kelly <tkelly3118@gmail.com>
2021-03-30 13:56:16 +0100

    Update AllSix.jsx

--- client/src/components/AllSix.jsx
+++ client/src/components/AllSix.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 import LineGraph from './LineGraph';
 import PieChart from './PieChart';
@@ -13,31 +14,37 @@ function AllSix({ lineData, pieData }) {
     <section className="AllSix">
       <Link to={`${url}/Application`}>
         <div className="graph Application">
+          Application
           <LineGraph data={lineData} smallGraph />
         </div>
       </Link>
       <Link to={`${url}/Consensus`}>
         <div className="graph Consensus">
+          Consensus
           <PieChart data={pieData} smallGraph />
         </div>
       </Link>
       <Link to={`${url}/Incentive`}>
         <div className="graph Incentive">
+          Incentive
           <PieChart data={pieData} smallGraph />
         </div>
       </Link>
       <Link to={`${url}/Operational`}>
         <div className="graph Operational">
+          Operational
           <LineGraph data={lineData} smallGraph />
         </div>
       </Link>
       <Link to={`${url}/Network`}>
         <div className="graph Network">
+          Network
           <ResponsiveChoropleth />
         </div>
       </Link>
       <Link to={`${url}/Governance`}>
         <div className="graph Governance">
+          Governance
           <PieChart data={pieData} smallGraph />
         </div>
       </Link>
