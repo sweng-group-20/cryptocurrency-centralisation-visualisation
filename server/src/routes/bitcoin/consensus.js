c8f064b01725e66d317c6e667836e311fc6ec293
Juuiko <mahonal@tcd.ie>
2021-03-30 17:17:16 +0200

    changed webscraping to use api

--- server/src/routes/bitcoin/consensus.js
+++ server/src/routes/bitcoin/consensus.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+const fetch = require('node-fetch');
 
 const logger = require('../../logger');
 
@@ -17,92 +16,28 @@ router.get('/', (_req, res) => {
 
 router.get('/data', async (_req, res) => {
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto('https://miningpoolstats.stream/bitcoin');
-
-    const html = await page.content();
-    const $ = cheerio.load(html);
-
-    let total = 0;
-    let stringTemp = '';
-
-    total = $('#poolshash').text();
-    total = total.substring(0, total.length - 5);
-    total = parseFloat(total);
-
     const poolNames = [];
     const poolHashs = [];
-
-    $('#pools')
-      .children('tbody')
-      .children('tr')
-      .each((i, elem) => {
-        // new line
-        stringTemp = $(elem).children('td').eq(7).text();
-        if (stringTemp.length === 0) {
-          poolHashs[i] = '';
-        } else if (
-          stringTemp.substring(0, 1) === '0' ||
-          stringTemp.substring(0, 1) === 'A'
-        ) {
-          return;
-        } else if (
-          stringTemp.substring(stringTemp.length - 4, stringTemp.length) ===
-          'EH/s'
-        ) {
-          poolHashs[i] = stringTemp.substring(0, stringTemp.length - 5);
-        } else if (
-          stringTemp.substring(stringTemp.length - 4, stringTemp.length) ===
-          'PH/s'
-        ) {
-          poolHashs[i] = (
-            stringTemp.substring(0, stringTemp.length - 5) / 1000
-          ).toString();
-        } else if (
-          stringTemp.substring(stringTemp.length - 4, stringTemp.length) ===
-          'TH/s'
-        ) {
-          poolHashs[i] = (
-            stringTemp.substring(0, stringTemp.length - 5) / 1000000
-          ).toString();
-        } else {
-          return;
-        }
-        poolNames[i] = $(elem)
-          .children('td')
-          .eq(1)
-          .children('div:first')
-          .children('b')
-          .text();
-      });
-    await browser.close();
-
     const pluginArrayArg = [];
 
-    let accum = 0;
+    const time = await fetch('https://miningpoolstats.stream/data/time');
+    const webDataRaw = await fetch(
+      `https://data.miningpoolstats.stream/data/bitcoin.js?t=${await time.text()}`
+    );
 
-    poolNames.forEach((_, i) => {
+    const webData = await webDataRaw.json();
+    webData.data.forEach((_, i) => {
+      poolNames[i] = webData.data[i].pool_id;
+      poolHashs[i] = (webData.data[i].hashrate / 1e15).toFixed(2);
+
       const jsonObj = {};
       jsonObj.id = poolNames[i];
       jsonObj.label = poolNames[i];
       jsonObj.value = poolHashs[i];
-
-      accum += poolHashs[i];
       pluginArrayArg.push(jsonObj);
     });
-
-    const leftOver = parseFloat(total) - parseFloat(accum);
-
-    const jsonObj = {};
-    jsonObj.id = 'independent';
-    jsonObj.label = 'independent';
-    jsonObj.value = leftOver.toFixed(2);
-    pluginArrayArg.unshift(jsonObj);
-
+    pluginArrayArg.sort((a, b) => a.value - b.value);
     const data = JSON.parse(JSON.stringify(pluginArrayArg));
-
     res.json({
       data,
     });
