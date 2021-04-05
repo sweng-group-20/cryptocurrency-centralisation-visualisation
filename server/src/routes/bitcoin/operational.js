a4dbca19966675aa598017e7847f88c10ce5f989
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2021-04-05 11:33:58 +0100

    Add error handling

--- server/src/routes/bitcoin/operational.js
+++ server/src/routes/bitcoin/operational.js
@@ -64,27 +64,31 @@ router.get('/', (_req, res) => {
  *                             y:
  *                               type: number
  */
-router.get('/storage-constraint', async (_req, res) => {
-  const resp = await fetch(
-    'https://api.blockchair.com/bitcoin/blocks?a=date,sum(size)',
-    { method: 'GET' }
-  );
-  const respJSON = await resp.json();
-  const respData = respJSON.data;
-  const compareInterval = 14; // days
-  const data = [{ id: 'Bitcoin', data: [] }];
-  for (let i = compareInterval; i < respData.length; i += 1) {
-    const sizeNow = respData[i]['sum(size)'];
-    const sizeBefore = respData[i - compareInterval]['sum(size)'];
-    data[0].data.push({
-      x: respData[i].date,
-      y: sizeNow / sizeBefore,
+router.get('/storage-constraint', async (_req, res, next) => {
+  try {
+    const resp = await fetch(
+      'https://api.blockchair.com/bitcoin/blocks?a=date,sum(size)',
+      { method: 'GET' }
+    );
+    const respJSON = await resp.json();
+    const respData = respJSON.data;
+    const compareInterval = 14; // days
+    const data = [{ id: 'Bitcoin', data: [] }];
+    for (let i = compareInterval; i < respData.length; i += 1) {
+      const sizeNow = respData[i]['sum(size)'];
+      const sizeBefore = respData[i - compareInterval]['sum(size)'];
+      data[0].data.push({
+        x: respData[i].date,
+        y: sizeNow / sizeBefore,
+      });
+    }
+    res.status(200);
+    res.json({
+      data,
     });
+  } catch (err) {
+    next(err);
   }
-  res.status(200);
-  res.json({
-    data,
-  });
 });
 
 module.exports = router;
